7985e87b54e66e7aea6d1be40a7356113a5edbb6
paliFrotan <mspaligill@gmail.com>
2026-03-01 13:53:46 +0000

    Keep scret variable

--- my_project/settings.py
+++ my_project/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--0-cn0!2(#w@vbocv%51ihqxgc@#l3r-x2k)e77a9ikhv+g$96'
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
